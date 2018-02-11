625d15a13edcb19cbc1b432b7834930b03fa8309
Alexander Kiel <alexanderkiel@gmx.net>
2018-02-11 15:07:38 +0100

    Bump Next Dev Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prom-metrics "0.2-alpha1"
+(defproject prom-metrics "0.3-SNAPSHOT"
   :description "Clojure Wrappers for the Prometheus Java Client."
   :url "https://github.com/alexanderkiel/prom-metrics"
   :license {:name "Eclipse Public License"
