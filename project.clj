110552874b4c408286a607e4a60ee7a61cfd8814
Alexander Kiel <alexanderkiel@gmx.net>
2018-08-14 19:36:54 +0200

    Move Clojure Dependency into Dev Profile
    We don't need a special Clojure dependency. Any Clojure since 1.6 will
    do.

--- project.clj
+++ project.clj
@@ -7,14 +7,24 @@
   :min-lein-version "2.6.0"
   :pedantic? :abort
 
-  :dependencies [[io.prometheus/simpleclient "0.5.0"]
-                 [io.prometheus/simpleclient_common "0.5.0"]
-                 [org.clojure/clojure "1.9.0"]]
+  :dependencies
+  [[io.prometheus/simpleclient "0.5.0"]
+   [io.prometheus/simpleclient_common "0.5.0"]]
 
-  :profiles {:dev {:dependencies [[criterium "0.4.4"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+  :profiles
+  {:dev
+   {:dependencies
+    [[criterium "0.4.4"]
+     [org.clojure/clojure "1.9.0"]]}
+   :1.8
+   {:dependencies
+    [[org.clojure/clojure "1.8.0"]]}
+   :1.7
+   {:dependencies
+    [[org.clojure/clojure "1.7.0"]]}
+   :1.6
+   {:dependencies
+    [[org.clojure/clojure "1.6.0"]]}}
 
   :aliases
   {"lint" ["eastwood" "{:linters [:all]}"]})
